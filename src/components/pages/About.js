d00bd21c4768514b639bef9d191a782c94d37e03
Kathy * <Kathy@KathyLambert.me>
2019-04-22 16:53:04 -0400

    Git merge error corrections in App.css.  Added About.js component in the newly created pages folder.

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+// Command Short-cut: `rfc`
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+        <p>This the the app the helps you to Keep Pushing towards your best SELF</p>
+    </React.Fragment>
+  )
+}
+
