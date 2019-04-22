27d7c99dafd9dd57a130d1d946f7f6a5f57443f5
Kathy * <Kathy@KathyLambert.me>
2019-04-22 18:07:32 -0400

    Links added to the Header.js and Logo.js components with styling.

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
+    <div className='header' >
       <h1 className='header-title'>Git Sh*t Done!</h1>
+      
+      <Link className='link' to='/'>Home</Link> | <Link className='link' to='about'>About</Link>
+
     </div>
   )
 }
--- src/components/layouts/Logo.js
+++ src/components/layouts/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
     <div>
-      <h4 className='logo'>CodeMeKathy</h4>
+      <Link className='link' to='/'><h4 className='logo'>CodeMeKathy</h4></Link>
     </div>
   )
 }
