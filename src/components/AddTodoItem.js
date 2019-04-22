9bc3c2ebcf0092c9b7c5c4f53ec0f6da75704bc9
Kathy * <Kathy@KathyLambert.me>
2019-04-22 15:00:52 -0400

    Renamed the input field within AddTodoItem.js to addTodoInput and moved the styling from to App.css.

--- src/components/AddTodoItem.js
+++ src/components/AddTodoItem.js
@@ -1,22 +1,44 @@
-// Short-cut Command: 'rce'
+// Command Short-cut : 'rce'
 
 import React, { Component } from 'react'
 
 export class AddTodoItem extends Component {
+  // Add a component level state to capture the user input into the AddTodoItem input and Confirm data pass thru.
+    state = {
+      title: ''
+    }
+ 
+    onSubmit = (e) => {
+      e.preventDefault();
+      this.props.addTodo(this.state.title)//pass the props for the method name created in App.js
+      this.setState({ title: '' }) //resets the state of the input field to blank after submission.
+    }
+
+    // Create the required `onChange` event handler to handle the title data state changes. An example of component level state.
+    onChange = (e) => this.setState({ title: e.target.value })
+
+
+
+    
   render() {
     return (
-      <div>
-        <form style={{display: 'flex' }}>
-          <input type="text"
-            name='name'
-            style={{ flex: '10', padding: '5px' }}
-            placeholder='Add New Todo'
-        />
+      <div>  
+        <form onSubmit={this.onSubmit} style={{display: 'flex' }}> 
+        {/* Add an event handler to handle the state changes submission when the submit btn selected. */}
+          <input 
+            type='text'
+            name='title'
+            className='addTodoInput'
+            // style={{ flex: '10', padding: '5px' }}
+            placeholder='Add New Todo...'
+            value={this.state.title}
+            onChange={this.onChange}
+          />
         <input 
         type="submit" 
         value="Submit"
         className='btn'
-        style={{flex: '10'}}
+        style={{flex: '1 '}}
         />
         </form>
       </div>
